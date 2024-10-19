e8ade329ca7c56c70c75dd8df77c6ef0fd5cfa75
anonymouscoder25 <149677844+anonymouscoder25@users.noreply.github.com>
2024-10-19 16:23:31 -0400

    delete character module and make changes

--- modules/animations-list/init.lua
+++ modules/animations-list/init.lua
@@ -1,25 +1,48 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local packages = ReplicatedStorage.Packages
+
+local trove = require(packages.Trove).new() --//MUST HAVE TROVE IN GAME LOL
+
 local AnimationsList = {}
-local animations = { --//Possibly make nested tables an array to improve performance
-    ["Slash"] = "1230183981" --eg
+AnimationsList.animations = { --index1 = id, index2 = animationTrack
+    ["Slash"] = "1230183981"
 }
 
-for i,id in ipairs(animations) do
-    animations[i] = "rbxassetid://"..id
+local function createAnimation(animId)
+    local animation = trove:Add(Instance.new("Animation"))
+    animation.AnimationId = animId
+    return animation
 end
 
-function AnimationsList:Hook(char,animIndex)
-    local animId = animations[animIndex]
-    if not animId then return end
+for i,id in ipairs(AnimationsList.animations) do
+    AnimationsList.animations[i] = {"rbxassetid://"..id}
+end
 
+function AnimationsList:Play(char,animIndex)
+    local animId = AnimationsList.animations[animIndex]
+    if not animId or animId[2] then return end
     local hum = char:FindFirstChild("Humanoid")
     local animator = hum.Animator
 
-    animations[animIndex][2] = animator:LoadAnimation(animId)
+    AnimationsList.animations[animIndex][2] = animator:LoadAnimation(animId)
 end
 
-function AnimationsList:Unhook()
-    for i,v in ipairs(animations) do
-        animations[i][2] = nil
+
+function AnimationsList.new(char)
+    trove:Destroy()
+    local folder = trove:Add(Instance.new("Folder"))
+    folder.Name = "Animations"
+    folder.Parent = ReplicatedStorage
+
+    local hum = char:FindFirstChild("Humanoid")
+    local animator = hum.Animator
+    for name,info in ipairs(AnimationsList.animations) do
+        local anim = createAnimation(info[1])
+        anim.Parent = folder
+        AnimationsList.animations[name] = {info[1],animator:LoadAnimation(anim)}
+
+        AnimationsList.Play(AnimationsList,char,name)
     end
 end
 
